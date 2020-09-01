62cd5899b47c317bb7490b3fdd1c020fde140c72
Santiago Trini <santiagotrini@gmail.com>
2020-08-31 21:54:17 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Aplicacion en construccion');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
